434094572886ccc758b836f7181d93140c648493
mikevhe18 <viriyananda.michael@gmail.com>
2020-09-29 15:39:21 +0700

    [8.0.2.0.0][attachment_multiple_records]

---
+++ attachment_multiple_records/hooks.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from openerp import SUPERUSER_ID
+
+
+def update_existing_attachment(cr, registry):
+    obj_ir_attachment = registry["ir.attachment"]
+    obj_ir_attachment_doc = registry["ir.attachment.document"]
+
+    attachment_ids = obj_ir_attachment.search(cr, SUPERUSER_ID, [], order="id")
+    attachment = obj_ir_attachment.browse(cr, SUPERUSER_ID, attachment_ids)
+    for data in attachment:
+        value = {
+            "res_id": data.res_id,
+            "res_model": data.res_model,
+            "res_name": data.res_name,
+            "attachment_id": data.id,
+        }
+        obj_ir_attachment_doc.create(cr, SUPERUSER_ID, value)
