7ce56a9d238727650eb5b5f542e57ff57cdec241
mikevhe18 <viriyananda.michael@gmail.com>
2020-10-15 16:32:42 +0700

    [8.0.2.0.0][opnsynid_knowledge] Revisi hooks.py

--- attachment_multiple_records/hooks.py
+++ attachment_multiple_records/hooks.py
@@ -9,15 +9,19 @@ def update_existing_attachment(cr, registry):
     obj_ir_attachment = registry["ir.attachment"]
     obj_ir_attachment_doc = registry["ir.attachment.document"]
 
-    attachment_ids = obj_ir_attachment.search(cr, SUPERUSER_ID, [], order="id")
-    attachment = obj_ir_attachment.browse(cr, SUPERUSER_ID, attachment_ids)
-    filtered_attachment = attachment.filtered(
-        lambda r: r.res_id and r.res_model)
-    for data in filtered_attachment:
-        value = {
-            "res_id": data.res_id,
-            "res_model": data.res_model,
-            "res_name": data.res_name,
-            "attachment_id": data.id,
-        }
-        obj_ir_attachment_doc.create(cr, SUPERUSER_ID, value)
+    criteria = [
+        ("res_id", "=", True),
+        ("res_model", "=", True),
+    ]
+    attachment_ids = \
+        obj_ir_attachment.search(cr, SUPERUSER_ID, criteria, order="id")
+    if attachment_ids:
+        attachment = obj_ir_attachment.browse(cr, SUPERUSER_ID, attachment_ids)
+        for data in attachment:
+            value = {
+                "res_id": data.res_id,
+                "res_model": data.res_model,
+                "res_name": data.res_name,
+                "attachment_id": data.id,
+            }
+            obj_ir_attachment_doc.create(cr, SUPERUSER_ID, value)
